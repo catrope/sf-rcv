3e42f69a42c355fdaa18d3e96c3b302408162ca2
Roan Kattouw <roan.kattouw@gmail.com>
2018-06-08 21:40:23 -0700

    tosqlite.js: Use sqlite3 driver instead of outputting SQL

--- tosqlite.js
+++ tosqlite.js
@@ -1,29 +1,24 @@
-const sqlite = require( 'sqlite3' ),
+const sqlite3 = require( 'sqlite3' ),
 	fs = require( 'fs' );
 
 if ( process.argv.length < 3 ) {
 	console.error( 'Usage: node tosqlite.js data.json' );
 }
 
-function encodeValue( x ) {
-	if ( typeof x === 'number' ) {
-		return x;
-	}
-	return "'" + String(x).replace(/'/g, "''") + "'";
-}
+const data = JSON.parse( fs.readFileSync( process.argv[2], { encoding: 'utf8' } ) ),
+	db = new sqlite3.Database( process.argv[2].replace( /\.json$/, '' ) + '.sqlite' );
 
-const data = JSON.parse( fs.readFileSync( process.argv[2], { encoding: 'utf8' } ) );
+db.serialize( function () {
+	db.run( 'CREATE TABLE ballots(id INT, contest TEXT, first TEXT, second TEXT, third TEXT, precinct TEXT, machine INT, tallyType TEXT)' );
+	db.run( 'BEGIN' );
 
-console.log('CREATE TABLE ballots(id INT, contest TEXT, first TEXT, second TEXT, third TEXT, precinct TEXT, machine INT, tallyType TEXT);');
-console.log('BEGIN;');
-for ( let contest in data ) {
-	for ( let id in data[contest] ) {
-		let vote = data[contest][id];
-		console.log( 'INSERT INTO ballots VALUES(' +
-			[ Number( id ), contest ].concat( vote.votes ).concat( [ vote.precinct, vote.machine, vote.tallyType ] )
-				.map( encodeValue ).join( ', ' ) +
-			');'
-		);
+	let statement = db.prepare( 'INSERT INTO ballots VALUES (?, ?, ?, ?, ?, ?, ?, ?)' );
+	for ( let contest in data ) {
+		for ( let id in data[contest] ) {
+			let vote = data[contest][id];
+			statement.run( Number( id ), contest, ...vote.votes, vote.precinct, vote.machine, vote.tallyType );
+		}
 	}
-}
-console.log('END;');
+	statement.finalize();
+	db.run( 'END' );
+} );
