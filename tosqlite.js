fa4ab813375269b47eec0f5f715eceff307a2ed3
Roan Kattouw <roan.kattouw@gmail.com>
2018-06-10 20:22:11 -0700

    Add roundOf3 and roundOf2 columns to DB

--- tosqlite.js
+++ tosqlite.js
@@ -1,5 +1,6 @@
 const sqlite3 = require( 'sqlite3' ),
-	fs = require( 'fs' );
+	fs = require( 'fs' ),
+	rcv = require( './rcv.js' );
 
 
 function formatVote( vote ) {
@@ -17,6 +18,17 @@ function getDistrict( precinct ) {
 	return secondDigit === 0 ? 10 : secondDigit;
 }
 
+function keyById( buckets ) {
+	let result = {};
+	for ( let candidate in buckets ) {
+		result[candidate] = {};
+		for ( let ballot of buckets[candidate] ) {
+			result[candidate][ballot.id] = ballot;
+		}
+	}
+	return result;
+}
+
 if ( process.argv.length < 4 ) {
 	console.error( 'Usage: node tosqlite.js data.json db.sqlite3' );
 }
@@ -24,18 +36,45 @@ if ( process.argv.length < 4 ) {
 const data = JSON.parse( fs.readFileSync( process.argv[2], { encoding: 'utf8' } ) ),
 	db = new sqlite3.Database( process.argv[3] );
 
+let contestData = {};
+for ( let contest in data ) {
+	let ballots = data[contest];
+	for ( let id in ballots ) {
+		ballots[id].id = id;
+	}
+	contestData[contest] = rcv.runContest( Object.values( ballots ), true );
+}
+
+
+
 db.serialize( function () {
-	db.run( 'CREATE TABLE ballots(id INT, contest TEXT, first TEXT, second TEXT, third TEXT, precinct TEXT, district INT, machine INT, tallyType TEXT)' );
+	db.run( 'CREATE TABLE ballots(id INT, contest TEXT, first TEXT, second TEXT, third TEXT, roundOf3 TEXT, roundOf2 TEXT, precinct TEXT, district INT, machine INT, tallyType TEXT)' );
 	db.run( 'BEGIN' );
 
-	let statement = db.prepare( 'INSERT INTO ballots VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)' );
+	let statement = db.prepare( 'INSERT INTO ballots VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)' );
 	for ( let contest in data ) {
+		let roundOf3 = keyById( contestData[contest][ contestData[contest].length - 2].buckets ),
+			roundOf2 = keyById( contestData[contest][ contestData[contest].length - 1].buckets );
 		for ( let id in data[contest] ) {
-			let vote = data[contest][id];
+			let vote = data[contest][id], r3Candidate, r2Candidate;
+			for ( let candidate in roundOf3 ) {
+				if ( id in roundOf3[candidate] ) {
+					r3Candidate = candidate;
+					break;
+				}
+			}
+			for ( let candidate in roundOf2 ) {
+				if ( id in roundOf2[candidate] ) {
+					r2Candidate = candidate;
+					break;
+				}
+			}
 			statement.run(
 				Number( id ),
 				contest,
 				...vote.votes.map( formatVote ),
+				r3Candidate,
+				r2Candidate,
 				vote.precinct,
 				getDistrict( vote.precinct ),
 				vote.machine,
